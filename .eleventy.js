4c995ddee071934728210e3c8743aa29e4802459
stefan judis <stefanjudis@gmail.com>
2022-06-26 17:25:05 +0200

    Style tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -18,8 +18,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode('browserEntry', (browser) => {
     return `
       <div class="entry">
-        <div class="entry__version"><span>${browser.current.version}</span></div>
         <img src="/logos/${browser.key}.svg" alt="${browser.name} logo" width="10" height="10">
+        <div class="entry__version"><span>${browser.current.version}</span></div>
         <a href="${browser.current.release_notes}" aria-label="${browser.name} ${browser.current.version} release notes">${browser.name} ${browser.current.version} was released ${browser.current.release_date}</a>.
       </div>
     `;
